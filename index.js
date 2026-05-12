50c8d3699e398107ece47916c16bf7eb99aea9c6
vitorpaulo727171-prog <vitorpaulo727171@gmail.com>
2026-05-12 17:07:48 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -7,40 +7,28 @@ const app = express();
 const URL =
 'https://vsonline.wuaze.com/atualiza_todos_status.php?token=VS_87443981';
 
-let executando = false;
-
 async function executar() {
-
-    if (executando) return;
-
-    executando = true;
 
     let browser;
 
     try {
 
-        console.log('INICIANDO');
+        console.log('ABRINDO');
 
         browser = await puppeteer.launch({
 
             args: chromium.args,
 
-            defaultViewport: chromium.defaultViewport,
-
             executablePath: await chromium.executablePath(),
 
-            headless: chromium.headless
+            headless: true
 
         });
 
         const page = await browser.newPage();
 
-        await page.setUserAgent(
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/124 Safari/537.36'
-        );
-
         await page.goto(URL, {
-            waitUntil: 'networkidle2',
+            waitUntil: 'domcontentloaded',
             timeout: 60000
         });
 
@@ -48,21 +36,18 @@ async function executar() {
 
         const texto = await page.evaluate(() => document.body.innerText);
 
-        console.log('RESPOSTA:');
         console.log(texto);
 
     } catch (e) {
 
         console.log('ERRO');
-        console.log(e);
+        console.log(e.message);
 
     } finally {
 
         if (browser) {
             await browser.close();
         }
-
-        executando = false;
     }
 }
 
@@ -73,16 +58,6 @@ app.get('/ping', async (req, res) => {
     res.send('OK');
 });
 
-app.get('/', (req, res) => {
-
-    res.send('Render Online');
-});
-
-setInterval(executar, 300000);
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-
-    console.log('Servidor iniciado');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('ONLINE');
 });
